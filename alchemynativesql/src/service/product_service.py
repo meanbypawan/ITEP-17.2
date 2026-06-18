efe52d5ec0c19e2bde44761db92a00290a9e83e3
mean <meanstack4@gmail.com>
2026-06-18 09:30:47 +0530

    alchemy assignment 2

--- alchemynativesql/src/service/product_service.py
+++ alchemynativesql/src/service/product_service.py
@@ -1,7 +1,5 @@
 from src.dao.product_dao import ProductDAO
 from src.model import Product
-
-
 class ProductService:
     def __init__(self,session):
         self.product_dao = ProductDAO(session)
@@ -11,5 +9,8 @@ class ProductService:
 
     def fetch_product(self):
         return self.product_dao.fetch_products()
+
     def fetch_expensive_product(self):
         return self.product_dao.fetch_expensive_product()
+
+
